f6a6f2be0e5f734c019fb4de73ad40bd566510a7
laptopez <zam22119@uvg.edu.gt>
2024-08-21 16:27:47 -0600

    feat: added function to verify when the player is moving

--- src/controls.rs
+++ src/controls.rs
@@ -1,28 +1,33 @@
 use minifb::{Key, Window};
 use crate::player::Player;
-use std::f32::consts::PI;
+use crate::audio::AudioPlayer;
 
-pub fn process_events(window: &Window, player: &mut Player, maze: &Vec<Vec<char>>, _block_size: usize) {
+pub fn process_events(window: &Window, player: &mut Player, maze: &Vec<Vec<char>>, block_size: usize, steps_player: &AudioPlayer) {
     const MOVE_SPEED: f32 = 0.1;
-    const ROTATION_SPEED: f32 = PI / 40.0;
+    const ROTATION_SPEED: f32 = std::f32::consts::PI / 30.0;
+    let mut moved = false;
 
-    if window.is_key_down(Key::Left) {
+    // Rotación del jugador con A y D
+    if window.is_key_down(Key::A) {
         player.a -= ROTATION_SPEED;
     }
-    if window.is_key_down(Key::Right) {
+    if window.is_key_down(Key::D) {
         player.a += ROTATION_SPEED;
     }
 
     let mut next_pos_x = player.pos.x;
     let mut next_pos_y = player.pos.y;
 
-    if window.is_key_down(Key::Up) {
+    // Movimiento del jugador con W y S
+    if window.is_key_down(Key::W) {
         next_pos_x += player.a.cos() * MOVE_SPEED;
         next_pos_y += player.a.sin() * MOVE_SPEED;
+        moved = true;
     }
-    if window.is_key_down(Key::Down) {
+    if window.is_key_down(Key::S) {
         next_pos_x -= player.a.cos() * MOVE_SPEED;
         next_pos_y -= player.a.sin() * MOVE_SPEED;
+        moved = true;
     }
 
     let next_cell_x = next_pos_x as usize;
@@ -32,4 +37,34 @@ pub fn process_events(window: &Window, player: &mut Player, maze: &Vec<Vec<char>
         player.pos.x = next_pos_x;
         player.pos.y = next_pos_y;
     }
+
+    // Reproducir o pausar el sonido de los pasos dependiendo si el jugador se mueve o no
+    if moved {
+        steps_player.play();
+    } else {
+        steps_player.pause();
+    }
+}
+
+// Función que comprueba si una posición está dentro de una pared o muy cerca de ella
+fn is_colliding(x: f32, y: f32, maze: &Vec<Vec<char>>, threshold: f32) -> bool {
+    let map_x = x.floor() as isize;
+    let map_y = y.floor() as isize;
+
+    if map_x < 0 || map_x >= maze[0].len() as isize || map_y < 0 || map_y >= maze.len() as isize {
+        return true;
+    }
+
+    let cell = maze[map_y as usize][map_x as usize];
+
+    if cell != ' ' {
+        let dist_x = x - map_x as f32;
+        let dist_y = y - map_y as f32;
+
+        if dist_x < threshold || dist_x > (1.0 - threshold) || dist_y < threshold || dist_y > (1.0 - threshold) {
+            return true;
+        }
+    }
+
+    false
 }
